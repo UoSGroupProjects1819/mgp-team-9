87105edabaffd155d07d4114eed6660e8e41aa60
Oshawatt123 <38458089+Oshawatt123@users.noreply.github.com>
2019-02-06 10:41:13 +0000

    Basic Player Movement

---
+++ game/Team 9 Project/Assets/Scripts/playerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+
+    public Rigidbody playerRigidbody;
+    public float moveModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            playerRigidbody.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")) * moveModifier;
+        }
+        else
+        {
+            playerRigidbody.velocity = Vector3.zero;
+        }
+    }
+}
