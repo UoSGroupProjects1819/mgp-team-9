1d7403f785ad0794a5ff49b6280b5344a4bdcba7
Oshawatt123 <38458089+Oshawatt123@users.noreply.github.com>
2019-03-20 10:04:47 +0000

    Menu Improvements

---
+++ game/Team 9 Project/Assets/menuButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menuButtonScript : MonoBehaviour
+{
+
+    public CanvasRenderer MainCanvas;
+    public CanvasRenderer OptionsCanvas;
+
+    public void loadStartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
