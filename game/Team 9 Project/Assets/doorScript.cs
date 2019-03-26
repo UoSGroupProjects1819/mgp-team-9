0791170cd7c516ae3d832215912ee191f0b7396e
S196852 <47323987+S196852@users.noreply.github.com>
2019-03-26 18:48:59 +0000

    LV 3,4,5

---
+++ game/Team 9 Project/Assets/doorScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(shieldScript))]
+public class doorScript : MonoBehaviour
+{
+    private bool open;
+    private enemySpawnControl EnemySpawnControl;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite doorOpenSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemySpawnControl = GameObject.Find("Spawner").GetComponent<enemySpawnControl>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            boxCollider.enabled = false;
+            // change the sprite
+            //spriteRenderer.sprite = doorOpenSprite;
+        }
+        else
+        {
+            StartCoroutine(checkIfWavesEnded());
+        }
+    }
+
+    private IEnumerator checkIfWavesEnded()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if(EnemySpawnControl.enemyCount == 0)
+        {
+            open = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(this.transform.position, new Vector3(1.5f, 1.5f, 1.5f));
+    }
+}
---
+++ game/Team 9 Project/Assets/enemySpawnControl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawnControl : MonoBehaviour
+{
+
+    public float numberOfWaves;
+    private float waveNumber = 0;
+    public bool enemiesMustBeDead = true;
+    public float enemiesPerWave;
+
+    public enum spawnType
+    {
+        waves,
+        enemyCount,
+    };
+
+    public spawnType SpawnType;
+
+    public float spawnTime;
+    public int maxEnemyCount;
+    public int enemyCount;
+    public GameObject enemy;
+
+    public GameObject[] spawnPoints;
+
+    private bool canSpawnEnemy = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SpawnType == spawnType.waves)
+        {
+            spawnWave();
+            StartCoroutine(countdownToNextWave());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpawnType == spawnType.enemyCount)
+        {
+            if (enemyCount < maxEnemyCount)
+            {
+                if (canSpawnEnemy)
+                {
+                    spawnEnemy();
+                    StartCoroutine(countdownToNextWave());
+                }
+            }
+        }
+        else if (SpawnType == spawnType.waves)
+        {
+            if (enemiesMustBeDead)
+            {
+                if (enemyCount != 0)
+                {
+                    canSpawnEnemy = false;
+                }
+            }
+            if (canSpawnEnemy && waveNumber < numberOfWaves)
+            {
+                spawnWave();
+                StartCoroutine(countdownToNextWave());
+            }
+        }
+    }
+
+    private void spawnEnemy()
+    {
+        Instantiate(enemy, spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position, Quaternion.identity);
+        enemyCount += 1;
+    }
+
+    private void spawnWave()
+    {
+        waveNumber += 1;
+        for (int i = 0; i < enemiesPerWave; i++)
+        {
+            spawnEnemy();
+        }
+    }
+
+    private IEnumerator countdownToNextWave()
+    {
+        canSpawnEnemy = false;
+        yield return new WaitForSeconds(spawnTime);
+        canSpawnEnemy = true;
+    }
+}
