65c3f3c5f18bce7c6c3e18e0cc939284f9c06ad5
Oshawatt123 <38458089+Oshawatt123@users.noreply.github.com>
2019-04-02 10:24:17 +0100

    Additions to the spawner
    Added the need to walk into the current room in order for spawning to begin

--- game/Team 9 Project/Assets/Scripts/Enemy Scripts/enemySpawnControl.cs
+++ game/Team 9 Project/Assets/Scripts/Enemy Scripts/enemySpawnControl.cs
@@ -26,44 +26,52 @@ public class enemySpawnControl : MonoBehaviour
     public GameObject[] spawnPoints;
 
     private bool canSpawnEnemy = true;
+    private BoxCollider2D roomArea;
+    private bool roomEntered = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (SpawnType == spawnType.waves)
+        // Old initial wave spawn
+        /*if (SpawnType == spawnType.waves)
         {
             spawnWave();
             StartCoroutine(countdownToNextWave());
-        }
+        }*/
+
+        roomArea = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (SpawnType == spawnType.enemyCount)
+        if (roomEntered)
         {
-            if (enemyCount < maxEnemyCount)
+            if (SpawnType == spawnType.enemyCount)
             {
-                if (canSpawnEnemy)
+                if (enemyCount < maxEnemyCount)
                 {
-                    spawnEnemy();
+                    if (canSpawnEnemy)
+                    {
+                        spawnEnemy();
+                        StartCoroutine(countdownToNextWave());
+                    }
+                }
+            }
+            else if (SpawnType == spawnType.waves)
+            {
+                if (enemiesMustBeDead)
+                {
+                    if (enemyCount != 0)
+                    {
+                        canSpawnEnemy = false;
+                    }
+                }
+                if (canSpawnEnemy && waveNumber < numberOfWaves)
+                {
+                    spawnWave();
                     StartCoroutine(countdownToNextWave());
                 }
-            }
-        }
-        else if (SpawnType == spawnType.waves)
-        {
-            if (enemiesMustBeDead)
-            {
-                if (enemyCount != 0)
-                {
-                    canSpawnEnemy = false;
-                }
-            }
-            if (canSpawnEnemy && waveNumber < numberOfWaves)
-            {
-                spawnWave();
-                StartCoroutine(countdownToNextWave());
             }
         }
     }
@@ -80,6 +88,14 @@ public class enemySpawnControl : MonoBehaviour
         for (int i = 0; i < enemiesPerWave; i++)
         {
             spawnEnemy();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            roomEntered = true;
         }
     }
 
