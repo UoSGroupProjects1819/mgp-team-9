48aa0ae5ef4ed3454f42a6169ce55dbc9c8084d1
Oshawatt123 <lewis.arnold@hotmail.co.uk>
2019-05-07 19:34:20 +0100

    Added to the UI with new assets

--- game/Team 9 Project/Assets/Scripts/Misc Scripts/menuButtonScript.cs
+++ game/Team 9 Project/Assets/Scripts/Misc Scripts/menuButtonScript.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class menuButtonScript : MonoBehaviour
 {
 
-    public CanvasRenderer MainCanvas;
-    public CanvasRenderer OptionsCanvas;
+    public CanvasGroup MainCanvas;
+    public CanvasGroup OptionsCanvas;
+
+    // is help screen showing
+    private bool helpful = false;
 
     public void loadStartLevel()
     {
@@ -22,5 +25,33 @@ public class menuButtonScript : MonoBehaviour
 
         // quits the game when in a standalone application
         Application.Quit();
+    }
+
+    public void LoadHelp()
+    {
+        if (helpful)
+        {
+            helpful = false;
+
+            MainCanvas.interactable = true;
+            MainCanvas.blocksRaycasts = true;
+            MainCanvas.alpha = 1;
+
+            OptionsCanvas.interactable = false;
+            OptionsCanvas.blocksRaycasts = false;
+            OptionsCanvas.alpha = 0;
+        }
+        else
+        {
+            helpful = true;
+
+            MainCanvas.interactable = false;
+            MainCanvas.blocksRaycasts = false;
+            MainCanvas.alpha = 0;
+
+            OptionsCanvas.interactable = true;
+            OptionsCanvas.blocksRaycasts = true;
+            OptionsCanvas.alpha = 1;
+        }
     }
 }
